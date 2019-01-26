9c78187978497c03040b818d0f78f36c49131f68
Vladimir Skopek <vladim.skopek@gmail.com>
2019-01-26 16:06:34 +0100

    make it better8

--- justchat/asgi.py
+++ justchat/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
-"""
-
-import os
-import django
-from channels.routing import get_default_application
-
+import channels
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "justchat.settings")
-django.setup()
-application = get_default_application()
+channel_layer = channels.asgi.get_channel_layer()
